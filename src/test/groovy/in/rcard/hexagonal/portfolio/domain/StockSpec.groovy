4e9fdc157e692b4604aad3e7825ef8308ec847b2
rcardin <riccardo.cardin@gmail.com>
2022-03-30 21:58:36 +0200

    Small refactoring on Stock Specification

--- src/test/groovy/in/rcard/hexagonal/portfolio/domain/StockSpec.groovy
+++ src/test/groovy/in/rcard/hexagonal/portfolio/domain/StockSpec.groovy
@@ -5,9 +5,9 @@ import spock.lang.Specification
 class StockSpec extends Specification {
     def "add should increase of the given quantity the owned stocks"() {
         given:
-            Stock appleStocks = new Stock("AAPL", 1000L)
+            var appleStocks = new Stock("AAPL", 1000L)
         when:
-            Stock updatedAppleStocks = appleStocks.add(100L)
+            var updatedAppleStocks = appleStocks.add(100L)
         then:
             updatedAppleStocks.name == "AAPL"
             updatedAppleStocks.owned == 1100L
